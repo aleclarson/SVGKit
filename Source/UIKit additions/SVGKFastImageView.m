5dbf59d8dfb6ee14fcde7566c17479863a2b1cd3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-26 11:12:17 -0500

    fix?: use image.bounds instead of tileSize
    ..when deciding if interpolation should be disabled.
    
    Note: I do not remember why this change was necessary 😬

--- Source/UIKit additions/SVGKFastImageView.m
+++ Source/UIKit additions/SVGKFastImageView.m
@@ -264,8 +264,8 @@
 			CGContextTranslateCTM(context, i * tileSize.width, k * tileSize.height );
 			CGContextScaleCTM( context, scaleConvertImageToView.width, scaleConvertImageToView.height );
 
-			if ((tileSize.width == 1.0 && scaleConvertImageToView.width > 1.0) ||
-			    (tileSize.height == 1.0 && scaleConvertImageToView.height > 1.0)) {
+			if ((imageBounds.size.width == 1.0 && scaleConvertImageToView.width > 1.0) ||
+			    (imageBounds.size.height == 1.0 && scaleConvertImageToView.height > 1.0)) {
 				CGContextSetInterpolationQuality(context, kCGInterpolationNone);
 			}
 			
